38545962391255d8f050a98aa30624ad6c267055
atharvadixit <atharvadixit15@gmail.com>
2021-06-09 16:16:39 +0530

    Added Company, Address APi call in frontend

--- src/Components/NodeExpress.js
+++ src/Components/NodeExpress.js
@@ -9,44 +9,77 @@ class NodeExpress extends Component{
         super(props);
 
         this.state = {
-            url: "https://node-express-react-server.herokuapp.com/getUsersData",
-            data: [], 
-            isClicked: false,
-            isLoading: false,
-            isLoaded: false
+            urlUsers: "https://node-express-react-server.herokuapp.com/getUsersData",
+            urlCompany: "https://node-express-react-server.herokuapp.com/getCompanyData",
+            data: [],
+            isUserLoading: false,
+            isCompanyLoading: false,
+            isUserLoaded: false,
+            isCompanyLoaded: false
         };
     }
 
-    makeApiCall = () =>{
-        fetch(this.state.url, {async: false}).then(res => res.json())
+    makeCompanyCall = () =>{
+        fetch(this.state.urlCompany, {async: false}).then(res => res.json())
         .then(
             (result) => {
                 this.setState({
                     data: result,
-                    isLoaded: true
+                    isCompanyLoaded: true
                 });
             }
         )
     }
 
-    fetchData = () =>{
+    fetchCompanyData = () =>{
 
         this.setState(
             {
-                isLoading:true
+                isCompanyLoading:true,
+                isUserLoaded: false
             }
         )
         
-        this.makeApiCall();
+        this.makeCompanyCall();
 
-        this.setState({
-            isClicked: true
-        })
 
         setTimeout(function() {
   
             this.setState({
-              isLoading : false,
+              isCompanyLoading : false,
+          });
+      }.bind(this), 200)
+    }
+    
+    
+    makeUsersCall = () =>{
+        fetch(this.state.urlUsers, {async: false}).then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    data: result,
+                    isUserLoaded: true
+                });
+            }
+        )
+    }
+
+    fetchUsersData = () =>{
+
+        this.setState(
+            {
+                isUserLoading:true,
+                isCompanyLoaded: false
+            }
+        )
+        
+        this.makeUsersCall();
+
+
+        setTimeout(function() {
+  
+            this.setState({
+              isUserLoading : false,
           });
       }.bind(this), 200)
     }
@@ -56,40 +89,71 @@ class NodeExpress extends Component{
             <div className="container">
                 <br/>
                 <h2>Click the below button to make API call to the Node-Express Server</h2>
-                <center>
-                <button className="btn btn-dark" onClick={this.fetchData}>
-                    Make API Call
-                </button>
-                </center>
+                <br/>
+                <div className="row">
+                    <div className="col-lg-6 col-lg-offset-8">
+                        <button className="btn btn-dark" onClick={this.fetchUsersData}>
+                            Get Users Data
+                        </button>
+                    </div>
+                    <div className="col-lg-6 col-lg-offset-4">
+                        <button className="btn btn-dark" onClick={this.fetchCompanyData}>
+                            Get Company Data
+                        </button>
+                    </div>
+                </div>
                 <br></br>
                 <div className="row">
                 <div className="container">
-                {this.state.isLoaded == false?<p></p>:
-                <Table striped bordered hover size="sm">
-                <thead>
-                  <tr>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Website</th>
-                    <th>Company</th>
-                    <th>City</th>
-                  </tr>
-                </thead>
-                <tbody>
-                    {this.state.data.map(user => (
-                        <tr>
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <td>{user.website}</td>
-                            <td>{user.company.name}</td>
-                            <td>{user.address.city}</td>
-                        </tr>
-                    ))}
-                </tbody>
-              </Table>
-            }
+                    {this.state.isUserLoaded == false?<p></p>:
+                    <Table striped bordered hover size="sm">
+                    <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Website</th>
+                        <th>Company</th>
+                        <th>City</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.data.map(user => (
+                            <tr>
+                                <td>{user.id}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.website}</td>
+                                <td>{user.company.name}</td>
+                                <td>{user.address.city}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+                }
+
+                {this.state.isCompanyLoaded == false?<p></p>:
+                                <Table striped bordered hover size="sm">
+                                <thead>
+                                <tr>
+                                    <th>Sr no</th>
+                                    <th>Company Name</th>
+                                    <th>Company Moto</th>
+                                    <th>Area of work</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    {this.state.data.map(user => (
+                                        <tr>
+                                            <td>{user.id}</td>
+                                            <td>{user.company.name}</td>
+                                            <td>{user.company.catchPhrase}</td>
+                                            <td>{user.company.bs}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </Table>
+                            }
             </div>
                 </div>
             </div>
--- src/Components/SpringBoot.js
+++ src/Components/SpringBoot.js
@@ -9,44 +9,77 @@ class SpringBoot extends Component{
         super(props);
 
         this.state = {
-            url: "https://glacial-headland-06013.herokuapp.com/getUserData",
-            data: [], 
-            isClicked: false,
-            isLoading: false,
-            isLoaded: false
+            urlUsers: "https://glacial-headland-06013.herokuapp.com/getUserData",
+            urlAddress: "https://glacial-headland-06013.herokuapp.com/getCompanyData",
+            data: [],
+            isUserLoading: false,
+            isAddressLoading: false,
+            isUserLoaded: false,
+            isAddressLoaded: false
         };
     }
 
-    makeApiCall = () =>{
-        fetch(this.state.url, {async: false}).then(res => res.json())
+    makeAddressCall = () =>{
+        fetch(this.state.urlAddress, {async: false}).then(res => res.json())
         .then(
             (result) => {
                 this.setState({
                     data: result,
-                    isLoaded: true
+                    isAddressLoaded: true
                 });
             }
         )
     }
 
-    fetchData = () =>{
+    fetchAddressData = () =>{
 
         this.setState(
             {
-                isLoading:true
+                isAddressLoading:true,
+                isUserLoaded: false
             }
         )
         
-        this.makeApiCall();
+        this.makeAddressCall();
 
-        this.setState({
-            isClicked: true
-        })
 
         setTimeout(function() {
   
             this.setState({
-              isLoading : false,
+              isAddressLoading : false,
+          });
+      }.bind(this), 200)
+    }
+    
+    
+    makeUsersCall = () =>{
+        fetch(this.state.urlUsers, {async: false}).then(res => res.json())
+        .then(
+            (result) => {
+                this.setState({
+                    data: result,
+                    isUserLoaded: true
+                });
+            }
+        )
+    }
+
+    fetchUsersData = () =>{
+
+        this.setState(
+            {
+                isUserLoading:true,
+                isAddressLoaded: false
+            }
+        )
+        
+        this.makeUsersCall();
+
+
+        setTimeout(function() {
+  
+            this.setState({
+              isUserLoading : false,
           });
       }.bind(this), 200)
     }
@@ -55,41 +88,78 @@ class SpringBoot extends Component{
         return (
             <div className="container">
                 <br/>
-                <h2>Click the below button to make API call to SpringBoot Server</h2>
-                <center>
-                <button className="btn btn-dark" onClick={this.fetchData}>
-                    Make API Call
-                </button>
-                </center>
+                <h2>Click the below button to make API call to the Node-Express Server</h2>
+                <br/>
+                <div className="row">
+                    <div className="col-lg-6 col-lg-offset-8">
+                        <button className="btn btn-dark" onClick={this.fetchUsersData}>
+                            Get Users Data
+                        </button>
+                    </div>
+                    <div className="col-lg-6 col-lg-offset-4">
+                        <button className="btn btn-dark" onClick={this.fetchAddressData}>
+                            Get Company Data
+                        </button>
+                    </div>
+                </div>
                 <br></br>
                 <div className="row">
                 <div className="container">
-                {this.state.isLoaded == false?<p></p>:
-                <Table striped bordered hover size="sm">
-                <thead>
-                  <tr>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Website</th>
-                    <th>Company</th>
-                    <th>City</th>
-                  </tr>
-                </thead>
-                <tbody>
-                    {this.state.data.map(user => (
-                        <tr>
-                            <td>{user.id}</td>
-                            <td>{user.name}</td>
-                            <td>{user.email}</td>
-                            <td>{user.website}</td>
-                            <td>{user.company.name}</td>
-                            <td>{user.address.city}</td>
-                        </tr>
-                    ))}
-                </tbody>
-              </Table>
-            }
+                    {this.state.isUserLoaded == false?<p></p>:
+                    <Table striped bordered hover size="sm">
+                    <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Website</th>
+                        <th>Company</th>
+                        <th>City</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.data.map(user => (
+                            <tr>
+                                <td>{user.id}</td>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.website}</td>
+                                <td>{user.company.name}</td>
+                                <td>{user.address.city}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+                }
+
+                {this.state.isAddressLoaded == false?<p></p>:
+                                <Table striped bordered hover size="sm">
+                                <thead>
+                                <tr>
+                                    <th>Sr no</th>
+                                    <th>Street</th>
+                                    <th>Suite</th>
+                                    <th>City</th>
+                                    <th>Zip Code</th>
+                                    <th>Latitude</th>
+                                    <th>Longitude</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    {this.state.data.map(user => (
+                                        <tr>
+                                            <td>{user.id}</td>
+                                            <td>{user.address.street}</td>
+                                            <td>{user.address.suite}</td>
+                                            <td>{user.address.city}</td>
+                                            <td>{user.address.zipcode}</td>
+                                            <td>{user.address.geo.lat}</td>
+                                            <td>{user.address.geo.lng}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </Table>
+                            }
             </div>
                 </div>
             </div>
